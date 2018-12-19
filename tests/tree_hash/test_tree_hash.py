60ff2d563622c3e10f9927deeb8d0f8d52edeab6
Chih Cheng Liang <chihchengliang@gmail.com>
2018-12-19 18:40:16 +0800

    fix tree hash uint512

--- tests/tree_hash/test_tree_hash.py
+++ tests/tree_hash/test_tree_hash.py
@@ -1,6 +1,12 @@
 import pytest
 from ssz.tree_hash.tree_hash import hash_tree_root
-from ssz.sedes import Boolean
+from ssz.sedes import (
+    Boolean,
+    uint16,
+    uint32_list,
+    uint512,
+)
+
 
 @pytest.mark.parametrize(
     'value,expected',
@@ -13,3 +19,16 @@ def test_boolean_serialize_values(value, expected):
     sedes = Boolean()
     assert hash_tree_root(value, sedes) == expected
 
+
+@pytest.mark.parametrize(
+    'value,sedes',
+    (
+        (0, uint16),
+        (56, uint16),
+        ([123, 456, 789], uint32_list),
+        (55665566, uint512),
+        ([], uint32_list)
+    ),
+)
+def test_rich_types_of_data(value, sedes):
+    assert len(hash_tree_root(value, sedes)) == 32
