3574930ee41ac1a7d16b9221c7ac8eb6da9004b9
Chih Cheng Liang <chihchengliang@gmail.com>
2018-12-24 18:46:25 +0800

    add abstract method for deserialize_segment

--- ssz/sedes/base.py
+++ ssz/sedes/base.py
@@ -13,3 +13,7 @@ class BaseSSZSedes(ABC):
     @abstractmethod
     def deserialize(self):
         raise NotImplementedError("SSZ Sedes classes must implement this property")
+
+    @abstractmethod
+    def deserialize_segment(self, data, start_index):
+        raise NotImplementedError("SSZ Sedes classes must implement this property")
