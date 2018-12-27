3aaf5e95fee85459094cead8f0b1f803707af828
Jannik Luhn <jannik.luhn@brainbot.com>
2018-12-27 22:51:51 +0100

    Hash: Inherit from BaseSedes

--- ssz/sedes/hash.py
+++ ssz/sedes/hash.py
@@ -1,56 +1,33 @@
-from ssz.exceptions import (
-    DeserializationError,
-    SerializationError,
+from typing import (
+    Union,
 )
 
+from eth_utils import (
+    encode_hex,
+)
 
-class Hash:
-    """
-    A sedes for hashes (hash<N>).
-    """
-    num_bytes = 0
+from ssz.exceptions import (
+    SerializationError,
+)
+from ssz.sedes.base import FixedSizedSedes
 
-    def __init__(self, num_bytes):
-        if num_bytes <= 0:
-            raise ValueError(
-                "Number of bytes should be non-negavtive"
-            )
 
-        self.num_bytes = num_bytes
+BytesOrByteArray = Union[bytes, bytearray]
 
-    def serialize(self, val):
-        if not isinstance(val, bytes):
-            raise SerializationError('Hash should be of type bytes', val)
-        if len(val) != self.num_bytes:
+
+class Hash(FixedSizedSedes[BytesOrByteArray, bytes]):
+
+    def serialize_content(self, value: BytesOrByteArray) -> bytes:
+        if len(value) != self.length:
             raise SerializationError(
-                "Can only serialize values of {} bytes".format(self.num_bytes),
-                val
+                f"Can only serialize values of exactly {len(value)} bytes, got"
+                f"{encode_hex(value)} which is {len(value)} bytes."
             )
 
-        return val
+        return value
 
-    def deserialize_segment(self, data, start_index):
-        """
-        Deserialize the data from the given start_index
-        """
-        # Make sure we have sufficient data for deserializing
-        if len(data) < self.num_bytes + start_index:
-            raise DeserializationError(
-                'Insufficient data for deserializing',
-                data
-            )
-        end_index = start_index + self.num_bytes
-        return data[start_index:end_index], end_index
-
-    def deserialize(self, data):
-        deserialized_data, end_index = self.deserialize_segment(data, 0)
-        if end_index != len(data):
-            raise DeserializationError(
-                'Data to be deserialized is too long',
-                data
-            )
-
-        return deserialized_data
+    def deserialize_content(self, content: bytes) -> bytes:
+        return content
 
 
 hash32 = Hash(32)
