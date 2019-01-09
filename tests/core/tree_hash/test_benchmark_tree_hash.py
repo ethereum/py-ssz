c8c00d2420cf937918de1966a6aac39cf6b4bd60
Chih Cheng Liang <chihchengliang@gmail.com>
2019-01-09 19:18:21 +0800

    add a benchmark
    lint
    
    lint again
    
    lint really hard again

---
+++ tests/core/tree_hash/test_benchmark_tree_hash.py
@@ -0,0 +1,97 @@
+import time
+
+import pytest
+
+from ssz.sedes import (
+    List,
+    Serializable,
+    hash32,
+    uint24,
+    uint64,
+    uint384,
+)
+from ssz.tree_hash.tree_hash import (
+    hash_tree_root,
+)
+
+
+class ValidatorRecord(Serializable):
+    fields = [
+        ('pubkey', uint384),
+        ('withdrawal_credentials', hash32),
+        ('randao_commitment', hash32),
+        ('randao_layers', uint64),
+        ('status', uint64),
+        ('latest_status_change_slot', uint64),
+        ('exit_count', uint64),
+        ('poc_commitment', hash32),
+        ('last_poc_change_slot', uint64),
+        ('second_last_poc_change_slot', uint64),
+    ]
+
+
+class CrosslinkRecord(Serializable):
+    fields = [
+
+        ('slot', uint64),
+        ('shard_block_root', hash32),
+    ]
+
+
+class ShardCommittee(Serializable):
+    fields = [
+        ('shard', uint64),
+        ('committee', List(uint24)),
+        ('total_validator_count', uint64),
+    ]
+
+
+class State(Serializable):
+    fields = [
+        ('validator_registry', List(ValidatorRecord)),
+        ('shard_and_committee_for_slots', List(List(ShardCommittee))),
+        ('latest_crosslinks', List(CrosslinkRecord)),
+    ]
+
+
+validator_record = ValidatorRecord(
+    pubkey=123,
+    withdrawal_credentials=b'\x56' * 32,
+    randao_commitment=b'\x56' * 32,
+    randao_layers=123,
+    status=123,
+    latest_status_change_slot=123,
+    exit_count=123,
+    poc_commitment=b'\x56' * 32,
+    last_poc_change_slot=123,
+    second_last_poc_change_slot=123,
+)
+crosslink_record = CrosslinkRecord(slot=12847, shard_block_root=b'\x67' * 32)
+crosslink_record_stubs = [crosslink_record for i in range(1024)]
+
+
+@pytest.fixture
+def make_state():
+    def make_state(num_validators):
+        shard_committee = ShardCommittee(
+            shard=1, committee=tuple(range(num_validators // 1024)),
+            total_validator_count=num_validators,
+        )
+        shard_committee_stubs = tuple(tuple(shard_committee for i in range(16)) for i in range(64))
+        state = State(
+            validator_registry=tuple(validator_record for i in range(num_validators)),
+            shard_and_committee_for_slots=shard_committee_stubs,
+            latest_crosslinks=crosslink_record_stubs,
+        )
+        return state
+    return make_state
+
+
+def test_performance_to_root_a_state(make_state):
+    state = make_state(num_validators=2**18)
+
+    a = time.time()
+
+    hash_tree_root(state)
+
+    assert time.time() - a <= 15
