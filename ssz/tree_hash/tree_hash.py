cb67299e142cb5f25be7cfc755e959ea25b0a6dd
Chih Cheng Liang <chihchengliang@gmail.com>
2019-01-09 18:02:34 +0800

    support container

--- ssz/tree_hash/tree_hash.py
+++ ssz/tree_hash/tree_hash.py
@@ -15,9 +15,8 @@ from ssz.exceptions import (
 from ssz.sedes import (
     Boolean,
     Hash,
-    UnsignedInteger,
     Serializable,
-    List,
+    UnsignedInteger,
 )
 from ssz.utils import (
     infer_sedes,
@@ -32,23 +31,35 @@ from .merkle_hash import (
 
 
 def hash_tree_root(input_object: Any, sedes: Any=None) -> Hash32:
+    result = _hash_tree_root(input_object, sedes)
+    if len(result) < 32:
+        return Hash32(result.ljust(32, b'\x00'))
+    return Hash32(result)
+
+
+def _hash_tree_root(input_object: Any, sedes: Any=None) -> bytes:
     if sedes is None:
         sedes = infer_sedes(input_object)
 
     if isinstance(sedes, (Boolean, Hash)):
-        return input_object
+        return sedes.serialize(input_object)
 
     if isinstance(sedes, UnsignedInteger):
-        val = input_object.to_bytes(sedes.num_bytes, 'big')
+        value = input_object.to_bytes(sedes.num_bytes, 'big')
         if sedes.num_bytes > 32:
-            return hash_eth2(val)
+            return hash_eth2(value)
         else:
-            return val
+            return value
 
     if isinstance(input_object, Serializable):
-        return hash_eth2(b''.join([hash_tree_root(input_object[field_name], field_sedes) for field_name, field_sedes in input_object._meta.fields]))
+        container_hashs = (
+            _hash_tree_root(input_object[field_name], field_sedes)
+            for field_name, field_sedes in input_object._meta.fields
+        )
+        return hash_eth2(b''.join(container_hashs))
 
-    if isinstance(input_object, abc.Iterable) or isinstance(input_object, List):
-        return merkle_hash([hash_tree_root(item, sedes.element_sedes) for item in input_object])
+    if isinstance(input_object, abc.Iterable):
+        input_items = tuple(_hash_tree_root(item, sedes.element_sedes) for item in input_object)
+        return merkle_hash(input_items)
 
     raise TreeHashException("Can't produce tree hash", input_object, sedes)
