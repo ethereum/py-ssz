e8aa7b66d32026769a642ee91abf87beab4ebb5c
Chih Cheng Liang <chihchengliang@gmail.com>
2019-01-10 19:41:55 +0800

    split tree_hash tests and add more container tests
    lint
    
    remove accidental commit

---
+++ tests/core/tree_hash/test_lists.py
@@ -0,0 +1,26 @@
+import pytest
+
+from ssz.sedes import (
+    hash32_list,
+    uint32_list,
+)
+from ssz.tree_hash.tree_hash import (
+    hash_tree_root,
+)
+
+
+@pytest.mark.parametrize(
+    'items,sedes',
+    (
+        # Specify values in Tuple
+        ((123, 456, 789, ), uint32_list),
+        (tuple(), uint32_list),
+        ((b'\x56' * 32, ), hash32_list),
+        ((b'\x56' * 32, ) * 100, hash32_list),
+        ((b'\x56' * 32, ) * 101, hash32_list),
+    ),
+)
+def test_iterables(items, sedes):
+    # Make sure Lists are also tested
+    for value in (items, list(items),):
+        assert len(hash_tree_root(value, sedes)) == 32
