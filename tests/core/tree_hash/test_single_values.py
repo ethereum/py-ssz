3c351f2458a1ed856076a578d09deeaf413c7465
Chih Cheng Liang <chihchengliang@gmail.com>
2019-01-21 21:46:51 +0800

    test tree hash uint

--- tests/core/tree_hash/test_single_values.py
+++ tests/core/tree_hash/test_single_values.py
@@ -1,9 +1,18 @@
+from hypothesis import (
+    given,
+    strategies as st,
+)
 import pytest
 
+from ssz.exceptions import (
+    SerializationError,
+)
 from ssz.sedes import (
     Boolean,
-    uint16,
-    uint512,
+    UnsignedInteger,
+)
+from ssz.tree_hash.hash_eth2 import (
+    hash_eth2,
 )
 from ssz.tree_hash.tree_hash import (
     hash_tree_root,
@@ -22,13 +31,35 @@ def test_boolean_serialize_values(value, expected):
     assert hash_tree_root(value, sedes) == expected
 
 
+@given(data=st.data())
 @pytest.mark.parametrize(
-    'value,sedes',
-    (
-        (0, uint16),
-        (56, uint16),
-        (55665566, uint512),
-    ),
+    'num_bits',
+    (8, 16, 24, 32, 40, 48, 56, 64, 128, 256),
 )
-def test_non_iterables(value, sedes):
-    assert len(hash_tree_root(value, sedes)) == 32
+def test_unsign_integers_less_than_32_bytes(data, num_bits):
+    uintn = UnsignedInteger(num_bits)
+    value = data.draw(
+        st.integers(
+            min_value=0,
+            max_value=2**num_bits - 1,
+        )
+    )
+    expected = value.to_bytes(num_bits // 8, 'big').ljust(32, b'\x00')
+    assert hash_tree_root(value, uintn) == expected
+
+
+@given(data=st.data())
+@pytest.mark.parametrize(
+    'num_bits',
+    (384, 512),
+)
+def test_unsign_integers_more_than_32_bytes(data, num_bits):
+    uintn = UnsignedInteger(num_bits)
+    value = data.draw(
+        st.integers(
+            min_value=0,
+            max_value=2**num_bits - 1,
+        )
+    )
+    expected = hash_eth2(value.to_bytes(num_bits // 8, 'big'))
+    assert hash_tree_root(value, uintn) == expected
