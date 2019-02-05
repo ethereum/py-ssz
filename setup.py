c85b5eb2834fcfeb8112935d3107d03391efc2d8
Jason Carver <ut96caarrs@snkmail.com>
2019-02-05 10:01:46 -0800

    Bump version: 0.1.0-alpha.0 → 0.1.0-alpha.1

--- setup.py
+++ setup.py
@@ -39,7 +39,7 @@ extras_require['dev'] = (
 setup(
     name='ssz',
     # *IMPORTANT*: Don't manually change the version here. Use `make bump`, as described in readme
-    version='0.1.0-alpha.0',
+    version='0.1.0-alpha.1',
     description="""ssz: Python implementation of the Simple Serialization encoding and decoding""",
     long_description_markdown_filename='README.md',
     author='Jason Carver',
