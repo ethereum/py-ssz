7520afe7451cbbceefaf849ffc87c0b606d645b6
Jannik Luhn <jannik@brainbot.com>
2019-03-21 19:04:24 +0100

    Update pass through methods in Serializable

--- ssz/sedes/serializable.py
+++ ssz/sedes/serializable.py
@@ -334,15 +334,19 @@ class SerializableBase(abc.ABCMeta):
             cls,
             name,
             bases,
-            dict(
-                field_props +
-                tuple(attrs.items())
-            ),
+            dict(field_props + tuple(attrs.items())),
         )
 
     #
     # Implement BaseSedes methods as pass-throughs to the container sedes
     #
+    @property
+    def is_static_sized(cls):
+        return cls._meta.container_sedes.is_static_sized
+
+    def get_static_size(cls):
+        cls._meta.container_sedes.get_static_size()
+
     def serialize(cls: Type[TSerializable], value: TSerializable) -> bytes:
         return cls._meta.container_sedes.serialize(value)
 
@@ -361,9 +365,6 @@ class SerializableBase(abc.ABCMeta):
 
     def consume_bytes(cls, data: bytes, start_index: int, num_bytes: int) -> Tuple[bytes, int]:
         return cls._meta.container_sedes.consume_bytes(data, start_index, num_bytes)
-
-    def intermediate_tree_hash(cls: Type[TSerializable], value: TSerializable) -> bytes:
-        return cls._meta.container_sedes.intermediate_tree_hash(value)
 
 
 # Make any class created with SerializableBase an instance of BaseSedes
