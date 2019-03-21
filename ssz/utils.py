0a739a29cd0e3c429863d21837b26088b8a03813
Jannik Luhn <jannik@brainbot.com>
2019-03-21 18:54:19 +0100

    Implement base class for composite types

--- ssz/utils.py
+++ ssz/utils.py
@@ -4,6 +4,13 @@ from collections.abc import (
     Sequence,
 )
 
+from ssz.constants import (
+    SIZE_PREFIX_SIZE,
+    MAX_CONTENT_SIZE,
+)
+from ssz.exceptions import (
+    SerializationError,
+)
 from ssz.sedes.base import (
     BaseSedes,
 )
@@ -65,3 +72,14 @@ def get_duplicates(values):
         for item, num in counts.items()
         if num > 1
     )
+
+
+def get_size_prefix(content: bytes) -> bytes:
+    return len(content).to_bytes(SIZE_PREFIX_SIZE, "little")
+
+
+def validate_content_size(content: bytes) -> None:
+    if len(content) >= MAX_CONTENT_SIZE:
+        raise SerializationError(
+            f"Content size is too large to be encoded in a {SIZE_PREFIX_SIZE} byte prefix",
+        )
