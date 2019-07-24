227d27b534afe84fdf9b7e6b5b838604e368d398
Hsiao-Wei Wang <hwwang156@gmail.com>
2019-07-24 14:42:40 +0800

    Remove byte_list

--- tests/misc/test_utils.py
+++ tests/misc/test_utils.py
@@ -45,12 +45,6 @@ def test_pack(values, packed):
     assert pack(values) == packed
 
 
-@given(st.binary())
-def test_pack_bytes(data):
-    byte_list = tuple(bytes([byte]) for byte in data)
-    assert pack_bytes(data) == pack(byte_list)
-
-
 @pytest.mark.parametrize(("chunks", "root"), (
     (
         (A_CHUNK,),
