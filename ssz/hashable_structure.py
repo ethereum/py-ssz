ffb782c9cdde311ca749919ded5b404ac85192cd
Jannik Luhn <jannik@brainbot.com>
2019-10-30 19:45:49 +0800

    Split append and update functions

--- ssz/hashable_structure.py
+++ ssz/hashable_structure.py
@@ -1,8 +1,19 @@
 import functools
 import itertools
-from typing import Any, Dict, Iterable, Iterator, List, Sequence, Tuple, TypeVar, Union
+from typing import (
+    Dict,
+    Generator,
+    Iterable,
+    Iterator,
+    List,
+    Sequence,
+    Tuple,
+    TypeVar,
+    Union,
+)
 
 from eth_typing import Hash32
+from eth_utils import to_dict, to_tuple
 from eth_utils.toolz import groupby, partition, pipe
 from pyrsistent import pvector
 from pyrsistent._transformations import transform
@@ -28,12 +39,27 @@ TElement = TypeVar("TElement")
 def update_element_in_chunk(
     original_chunk: Hash32, index: int, element: bytes
 ) -> Hash32:
+    """Replace part of a chunk with a given element.
+
+    The chunk is interpreted as a concatenated sequence of equally sized elements. This function
+    replaces the element given by its index in the chunk with the given data.
+
+    If the length of the element is zero or not a divisor of the chunk size, a `ValueError` is
+    raised. If the index is out of range, an `IndexError is raised.
+
+    Example:
+        >>> update_element_in_chunk(b"aabbcc", 1, b"xx")
+        b'aaxxcc'
+    """
     element_size = len(element)
     chunk_size = len(original_chunk)
+
+    if element_size == 0:
+        raise ValueError(f"Element size is zero")
     if chunk_size % element_size != 0:
         raise ValueError(f"Element size is not a divisor of chunk size: {element_size}")
     if not 0 <= index < chunk_size // element_size:
-        raise ValueError(f"Index out of range for element size {element_size}: {index}")
+        raise IndexError(f"Index out of range for element size {element_size}: {index}")
 
     first_byte_index = index * element_size
     last_byte_index = first_byte_index + element_size
@@ -46,6 +72,11 @@ def update_element_in_chunk(
 def update_elements_in_chunk(
     original_chunk: Hash32, updated_elements: Dict[int, bytes]
 ) -> Hash32:
+    """Update multiple elements in a chunk.
+
+    The set of updates is given by a dictionary mapping indices to elements. The items of the
+    dictionary will be passed one by one to `update_element_in_chunk`.
+    """
     return pipe(
         original_chunk,
         *(
@@ -55,67 +86,89 @@ def update_elements_in_chunk(
     )
 
 
-def get_updated_and_appended_chunks(
+def get_num_padding_elements(
+    num_original_chunks: int, num_original_elements: int, element_size: int
+) -> int:
+    """Compute the number of elements that would still fit in the empty space of the last chunk."""
+    total_size = num_original_chunks * CHUNK_SIZE
+    used_size = num_original_elements * element_size
+    padding_size = total_size - used_size
+    num_elements_in_padding = padding_size // element_size
+    return num_elements_in_padding
+
+
+@to_dict
+def get_updated_chunks(
     updated_elements: Dict[int, bytes],
-    appended_elements: List[bytes],
+    appended_elements: Sequence[bytes],
     original_chunks: Sequence[Hash32],
     num_original_elements: int,
-) -> Tuple[Dict[int, Hash32], List[Hash32]]:
-    if not updated_elements and not appended_elements:
-        return {}, []
+    num_padding_elements: int,
+) -> Generator[Tuple[int, Hash32], None, None]:
+    """For an element changeset, compute the updates that have to be applied to the existing chunks.
 
+    The changeset is given as a dictionary of element indices to updated elements and a sequence of
+    appended elements. Note that appended elements that do not affect existing chunks are ignored.
+
+    The pre-existing state is given by the sequence of original chunks and the number of elements
+    represented by these chunks.
+
+    The return value is a dictionary mapping chunk indices to chunks.
+    """
+    effective_appended_elements = appended_elements[:num_padding_elements]
+
+    # get some element to infer the element size
     try:
         some_element = next(iter(updated_elements.values()))
     except StopIteration:
-        some_element = appended_elements[0]
+        try:
+            some_element = effective_appended_elements[0]
+        except IndexError:
+            return  # changeset is empty, so no chunks are updated
     element_size = len(some_element)
     elements_per_chunk = CHUNK_SIZE // element_size
 
-    # Find the elements whose chunk already exists. This is the case for all updated elements, but
-    # possibly also for the first few appended elements (if the original last chunk was not filled
-    # completely but zero-padded)
-    padding_length = (
-        len(original_chunks) * CHUNK_SIZE - num_original_elements * element_size
-    )
-    num_elements_in_padding = padding_length // element_size
     padding_elements_with_indices = dict(
-        enumerate(
-            appended_elements[:num_elements_in_padding], start=num_original_elements
-        )
+        enumerate(effective_appended_elements, start=num_original_elements)
     )
     effective_updated_elements = {**updated_elements, **padding_elements_with_indices}
 
-    # Compute the updated chunks
     element_indices = effective_updated_elements.keys()
     element_indices_by_chunk = groupby(
         lambda element_index: element_index // elements_per_chunk, element_indices
     )
-    in_chunk_updates_by_chunk = {
-        chunk_index: {
+
+    for chunk_index, element_indices in element_indices_by_chunk.items():
+        chunk_updates = {
             element_index
             % elements_per_chunk: effective_updated_elements[element_index]
             for element_index in element_indices
         }
-        for chunk_index, element_indices in element_indices_by_chunk.items()
-    }
-    updated_chunks = {
-        chunk_index: update_elements_in_chunk(
-            original_chunks[chunk_index], in_chunk_updates
+        updated_chunk = update_elements_in_chunk(
+            original_chunks[chunk_index], chunk_updates
         )
-        for chunk_index, in_chunk_updates in in_chunk_updates_by_chunk.items()
-    }
+        yield chunk_index, updated_chunk
 
-    # Compute appended chunks by packing the remaining elements
+
+@to_tuple
+def get_appended_chunks(
+    appended_elements: Sequence[bytes], num_padding_elements: int
+) -> Generator[Hash32, None, None]:
+    """Get the sequence of appended chunks."""
+    if len(appended_elements) <= num_padding_elements:
+        return
+
+    some_element = appended_elements[0]
+    element_size = len(some_element)
+    elements_per_chunk = CHUNK_SIZE // element_size
+
     chunk_partitioned_elements = partition(
         elements_per_chunk,
-        appended_elements[num_elements_in_padding:],
+        appended_elements[num_padding_elements:],
         pad=b"\x00" * element_size,
     )
-    appended_chunks = [
-        Hash32(b"".join(elements)) for elements in chunk_partitioned_elements
-    ]
-
-    return updated_chunks, appended_chunks
+    for elements_in_chunk in chunk_partitioned_elements:
+        yield Hash32(b"".join(elements_in_chunk))
 
 
 class BaseHashableStructure(HashableStructureAPI[TElement]):
@@ -136,14 +189,7 @@ class BaseHashableStructure(HashableStructureAPI[TElement]):
             sedes.serialize_element_for_tree(index, element)
             for index, element in enumerate(elements)
         ]
-        updated_chunks, appended_chunks = get_updated_and_appended_chunks(
-            {}, serialized_elements, [], 0
-        )
-        if updated_chunks:
-            raise Exception(
-                "Invariant: No chunks exist yet, so nothing that can be updated"
-            )
-
+        appended_chunks = get_appended_chunks(serialized_elements, 0, 0)
         hash_tree = HashTree.compute(
             appended_chunks or [ZERO_BYTES32], sedes.chunk_count
         )
@@ -250,18 +296,28 @@ class HashableStructureEvolver(HashableStructureEvolverAPI[TStructure, TElement]
                 self._appended_elements, start=len(self._original_structure)
             )
         ]
-        updated_chunks, appended_chunks = get_updated_and_appended_chunks(
+
+        updated_chunks = get_updated_chunks(
             updated_elements,
             appended_elements,
             self._original_structure.hash_tree.chunks,
             len(self._original_structure),
         )
+        appended_chunks = get_appended_chunks(
+            appended_elements,
+            len(self._original_structure.hash_tree.chunks),
+            len(self._original_structure),
+        )
 
         elements = self._original_structure.elements.mset(
-            *itertools.chain(*self._updated_elements.items())
+            *itertools.chain.from_iterable(  # type: ignore
+                self._updated_elements.items()
+            )
         ).extend(self._appended_elements)
         hash_tree = self._original_structure.hash_tree.mset(
-            *itertools.chain(*updated_chunks.items())
+            *itertools.chain.from_iterable(  # type: ignore
+                updated_chunks.items()
+            )
         ).extend(appended_chunks)
 
         return self._original_structure.__class__(
