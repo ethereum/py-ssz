55930d5179deee76991a484c6b867bc6a7d2792e
Jannik Luhn <jannik@brainbot.com>
2019-12-05 17:56:55 +0700

    Update flake8 version

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ extras_require = {
         "ruamel.yaml==0.15.87",
         "mypy-extensions>=0.4.1,<1.0.0",
     ],
-    "lint": ["flake8==3.4.1", "isort==4.3.21", "black==19.3b"],
+    "lint": ["flake8==3.7.8", "isort==4.3.21", "black==19.3b"],
     "doc": ["Sphinx>=1.6.5,<2", "sphinx_rtd_theme>=0.1.9"],
     "dev": ["bumpversion>=0.5.3,<1", "wheel", "twine", "ipython", "pre-commit==1.18.3"],
 }
